601bc23c485114b40cb453dc1fc661c02cca054b
Ismael Paul <ismaafp@gmail.com>
2022-06-05 17:51:49 +0100

    Created updateGrid() function that runs every time the slider moves
    This function aims to update the grid size (columns x rows) getting values from the slider

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const slider = document.getElementById("range-slider");
 const output = document.getElementById("range-grid");
 output.textContent = slider.value;
 
-let gridSize = slider.value
-
+// displays slider value
 slider.oninput = function() {
   output.textContent = this.value;
 }
 
-// create grid
+// create grid default grid 16 x 16
 const grid = document.getElementById("container-grid")
 
 const createGrid = () => {
     for(let i = 0; i < 256; i++) {
+
         const div = document.createElement("div");
         div.classList.add("grid-square");
         grid.appendChild(div);
@@ -22,3 +22,26 @@ const createGrid = () => {
 };
 
 createGrid();
+
+const updateGrid = () => {
+
+    let gridSize = slider.value * slider.value;
+
+    // resets the grid to not overwrite it
+    grid.innerHTML = "";
+
+    // changes columns and rows properties on CSS
+    grid.style.setProperty("grid-template-columns", `repeat(${slider.value}, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${slider.value}, 2fr)`);
+
+    // display the new grid based on slider value
+    for(let i = 0; i < gridSize; i++) {
+
+        const div = document.createElement("div");
+        div.classList.add("grid-square");
+        grid.appendChild(div);
+    }
+}
+
+// update the grid when slider is changed by the user
+slider.addEventListener("input", updateGrid);
